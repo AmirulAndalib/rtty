512fad1142c9c6a20b6bba0b19a81d3e23cd0323
Amirul Islam <amirulandalib9@gmail.com>
2022-03-07 14:03:01 +0600

    Update railwayfck.sh

--- railwayfck.sh
+++ railwayfck.sh
@@ -1,1 +1,1 @@
-apt install git wget rclone aria2c curl p7zip-full p7zip-rar unzip zip golang python3 python3-pip -y
+apt install git wget rclone aria2 curl p7zip-full p7zip-rar unzip zip golang python3 python3-pip -y
